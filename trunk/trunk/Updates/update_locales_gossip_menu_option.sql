888e939f6b3b8266b60a77b90cf2f90c5cd793b3
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2011-04-22 11:57:11 +0000

    update_locales_gossip_menu_option

---
+++ trunk/trunk/Updates/update_locales_gossip_menu_option.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8;                        
+                                       
+UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Je veux parcourir vos biens' WHERE `menu_id` =0 and id=1 ;
+UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Je veux voyager rapidement' WHERE `menu_id` =0 and id=2 ;
+UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Entrainez moi' WHERE `menu_id` =0 and id=3 ;
+UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Ramenez moi à la vie' WHERE `menu_id` =0 and id=4 ;
+UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Ramenez moi à la vie' WHERE `menu_id` =0 and id=5 ;
+
+
+
+
