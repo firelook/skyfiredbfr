cf5dc378b8eaaa610ce6c3b9af401e5984bf6aaf
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2011-04-24 05:39:29 +0000

    update_locales_achievement_reward

---
+++ trunk/Updates/update_locales_achievement_reward.sql
@@ -0,0 +1,11 @@
+/*SET NAMES utf8;
+*/
+
+
+
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Vous avez bien bourlingué !', `text_loc2` = 'Eh ben, vous !\n\nEt moi qui croyais avoir tout vu sur ces terres gelées ! Le feu de l\'exploration brûle en vous. Cela saute à mes yeux de nain.\n\nPortez ce tabard avec fierté. De cette façon, vos amis sauront toujours à qui demander leur chemin en temps voulu !\n\nRestez bien en selle !\n\nBrann Barbe-de-bronze' WHERE `entry` = 45;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Pour l\'Alliance !', `text_loc2` = 'La guerre fait rage sur nos terres. Seuls les héros les plus courageux osent frapper la Horde en ses points les plus sensibles. Vous êtes l\'un d\'eux.\n\nLes coups que vous avez portés au commandement de la Horde vont nous permettre de lancer notre assaut final. La Horde fléchira sous la puissance de l\'Alliance.\n\nVos actes seront récompensés. Chevauchez avec fierté !\n\n— Votre roi' WHERE `entry` = 614;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Lectures studieuses', `text_loc2` = 'Félicitations ! Vous avez terminé d\'étudier « Les écoles de magie des arcanes ». En récompense de votre dévouement, veuillez trouver ci-joint le volume spécial qui termine la collection.\n\nJe pense que vous trouverez ce tome particulièrement divertissant. Mais je vous laisse le découvrir par vous-même.\n\nCordialement,\n\nRhonin' WHERE `entry` = 1956;
+UPDATE `locales_achievement_reward` SET `subject_loc2` = 'Groupe improvisé', `text_loc2` = '$gCher membre assidu:Chère membre assidue;,\n\nnous aimerions récompenser votre ténacité à parcourir les donjons avec des personnes que vous n\'aviez probablement jamais rencontrées auparavant. Vous avez peut-être même appris les ficelles du métier à quelques petits nouveaux.\n\nBref, nous avons entendu dire que vous aimiez les groupes improvisés. Alors voici le petit Groopy, qui proclamera à tout le monde : « Vous savez quoi ? J\'suis groupé ! »\n\nAvec toute notre affection,\n\nVos amis de l\'équipe de développement de WoW' WHERE `entry` = 4478;
+
+
