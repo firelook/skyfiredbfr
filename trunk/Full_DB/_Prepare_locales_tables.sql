7965627f0ead459efb2078160bb807fba1a63d82
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2011-05-01 07:23:30 +0000

    update_locales_gameobject

--- trunk/Full_DB/_Prepare_locales_tables.sql
+++ trunk/Full_DB/_Prepare_locales_tables.sql
@@ -7,11 +7,13 @@ INSERT IGNORE INTO `locales_creature` (`entry`) SELECT `entry` FROM `creature_te
 INSERT IGNORE INTO `locales_quest`(`entry`) SELECT `entry` FROM `quest_template`;
 INSERT IGNORE INTO `locales_item_set_names` (`entry`) SELECT `entry` FROM `item_set_names`;
 INSERT IGNORE INTO `locales_item` (`entry`) SELECT `entry` FROM `item_template`;
-
-
-INSERT IGNORE INTO `locales_creature` (`entry`) SELECT `entry` FROM `creature_template`;
 INSERT IGNORE INTO `locales_page_text`(`entry`) SELECT `entry` FROM `page_text`;
 INSERT IGNORE INTO `locales_npc_text` (`entry`) SELECT `ID` FROM `npc_text`;
 INSERT IGNORE INTO `locales_points_of_interest` (`entry`) SELECT `entry` FROM `points_of_interest`;
 INSERT IGNORE INTO `locales_achievement_reward` (`entry`) SELECT `entry` FROM `achievement_reward`;
 INSERT IGNORE INTO `locales_gossip_menu_option` (`menu_id`, `id`) SELECT `menu_id`, `id` FROM `gossip_menu_option`;
+
+
+
+
+
