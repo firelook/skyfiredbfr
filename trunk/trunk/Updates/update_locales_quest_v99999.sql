7687cb4166c890e0d21e5e9432cdc4c18d4b900e
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2011-07-17 11:58:53 +0000

    update au 14/7/2011

---
+++ trunk/trunk/Updates/update_locales_quest_v99999.sql
@@ -0,0 +1,11 @@
+/*
+sET NAMES utf8;      
+*/
+
+/* 
+#############################################
+script modifications manuelles
+############################################# 
+*/ 
+
+UPDATE `locales_quest` SET `Title_loc2` = 'La quête de Kanrethad' ,`Details_loc2` = 'Bon retour du monde des morts. Il y a bien longtemps, vous étiez un héro parmi les vôtres. Aujourd’hui, vous êtes déchu, une relique du passé. Tenez-vous devant moi, pendant que j’éveille la légende que vous étiez autrefois. Je vous rendrai les armes et les pouvoirs que vous avez perdus, et vous serez à nouveau <complet/complète>.' ,`OfferRewardText_loc2` = 'Parlez à Kanrethad pour que vos talents, votre arme et votre monture vous soient rendus.' WHERE `entry` = 1;
