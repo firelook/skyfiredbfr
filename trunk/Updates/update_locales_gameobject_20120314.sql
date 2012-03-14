02aaafc7790ff61f4e0093441d2a0a45f04ddb12
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2012-03-14 09:35:32 +0000

    creature

--- trunk/Updates/update_locales_gameobject_20120314.sql
+++ trunk/Updates/update_locales_gameobject_20120314.sql
@@ -22,3 +22,10 @@ scripts créés à partir du pl de seraphinchaos
 UPDATE `locales_gameobject` SET `name_loc2` = 'Fleur de serpent' WHERE `entry` = 13874;
 */
 
+
+UPDATE `locales_gameobject` SET `name_loc2` = 'Feu de joie de l\'Alliance' WHERE `entry` = 207993;
+UPDATE `locales_gameobject` SET `name_loc2` = 'Coffre' WHERE `entry` = 208420;
+UPDATE `locales_gameobject` SET `name_loc2` = 'Pile vaudou' WHERE `entry` = 208549;
+UPDATE `locales_gameobject` SET `name_loc2` = 'Pile vaudou' WHERE `entry` = 208550;
+UPDATE `locales_gameobject` SET `name_loc2` = 'Bannière de la mine' WHERE `entry` = 208782;
+UPDATE `locales_gameobject` SET `name_loc2` = 'Bannière du moulin à eau' WHERE `entry` = 208785;
