fecbbfdf7fe4b23f429ea7a533fa100f5cd6e9f2
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2012-03-14 08:22:30 +0000

    local creature

---
+++ trunk/Updates/update_locales_gameobject_20120314.sql
@@ -0,0 +1,24 @@
+/*
+SET NAMES utf8;                        
+*/
+
+/*
+
+2012/03/14
+
+*/
+
+/* 
+#############################################
+scripts créés à partir du pl de seraphinchaos
+############################################# 
+*/ 
+
+/*#############################################
+#Parsing Gameobject de Wowhead 
+#############################################*/
+
+/*
+UPDATE `locales_gameobject` SET `name_loc2` = 'Fleur de serpent' WHERE `entry` = 13874;
+*/
+
