bd57fe742b39f6d1d74cd834204591630ad5506f
mrelfire@gmail.com <mrelfire@gmail.com@5ebed883-d49f-1e72-0acd-a3d316a71639>
2012-03-21 14:53:57 +0000

    sqlbot 5

---
+++ trunk/trunk/sqlbot/sqlbot_5_0.sql
@@ -0,0 +1,59 @@
+SET  @newENTRY =(SELECT max(`id`) FROM `auctionhouse` );
+set @vendeur_ah := 55;
+SET  @ENTRY =(SELECT max(`guid`) FROM `item_instance` );
+
+DROP TABLE IF EXISTS `t`;
+CREATE TABLE `t` (i INT);
+insert into `t` values(23442) , (79707) ,(4656);
+set  @faction_ah = (SELECT i FROM t ORDER BY RAND() LIMIT 1);
+
+
+
+
+INSERT INTO `item_instance` VALUES (	@ENTRY+1	,	17030	,	@vendeur_ah	,	0	,	0	,	1	,	0	,	'0 0 0 0 0 '	,	0	,	'0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 '	,	0	,	0	,	0	,	""	);
+INSERT INTO `item_instance` VALUES (	@ENTRY+2	,	63388	,	@vendeur_ah	,	0	,	0	,	floor(rand()*5+5)	,	0	,	'0 0 0 0 0 '	,	0	,	'0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 '	,	0	,	0	,	0	,	""	);
+INSERT INTO `item_instance` VALUES (	@ENTRY+3	,	64670	,	@vendeur_ah	,	0	,	0	,	floor(rand()*5+5)	,	0	,	'0 0 0 0 0 '	,	0	,	'0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 '	,	0	,	0	,	0	,	""	);
+
+
+
+
+
+
+INSERT INTO `auctionhouse` VALUES (	@newENTRY+1	,	@faction_ah	,	@ENTRY+1	,	@vendeur_ah	,	0	,	 UNIX_TIMESTAMP( ADDDATE(now(), INTERVAL floor(24+24*rand()) HOUR)) 	,	0	,	0	,	3037	,	0	);
+INSERT INTO `auctionhouse` VALUES (	@newENTRY+2	,	@faction_ah	,	@ENTRY+2	,	@vendeur_ah	,	0	,	 UNIX_TIMESTAMP( ADDDATE(now(), INTERVAL floor(24+24*rand()) HOUR)) 	,	0	,	0	,	2936	,	0	);
+INSERT INTO `auctionhouse` VALUES (	@newENTRY+3	,	@faction_ah	,	@ENTRY+3	,	@vendeur_ah	,	0	,	 UNIX_TIMESTAMP( ADDDATE(now(), INTERVAL floor(24+24*rand()) HOUR)) 	,	0	,	0	,	7968	,	0	);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
