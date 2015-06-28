02e1cec37602a439d3304337fb56f6ed3999c09e
MrElFire <mrelfire@firelook.com>
2015-06-28 06:52:57 +0200

    zzz

---
+++ trunk/Updates/434/zzz_Fix accents_game_event.sql
@@ -0,0 +1,32 @@
+-- For those who have a problems with the accents.
+-- @ made by sulbutx + Firelook
+
+
+UPDATE `game_event` SET description = replace(description,'Ã¢','â');
+UPDATE `game_event` SET description = replace(description,'Ã©','é');
+UPDATE `game_event` SET description = replace(description,'Ã»','û');
+UPDATE `game_event` SET description = replace(description,'Ã¯','ï');
+UPDATE `game_event` SET description = replace(description,'Ãª','ê');
+UPDATE `game_event` SET description = replace(description,'Ã¨','è');
+UPDATE `game_event` SET description = replace(description,'Ã®','î');
+UPDATE `game_event` SET description = replace(description,'Ã´','ô');
+UPDATE `game_event` SET description = replace(description,'ÃƒÂ©','é');
+UPDATE `game_event` SET description = replace(description,'Ã€','à');
+UPDATE `game_event` SET description = replace(description,'â€™',"'");
+UPDATE `game_event` SET description = replace(description,'â€“','à');
+UPDATE `game_event` SET description = replace(description,'â€¦','!');
+UPDATE `game_event` SET description = replace(description,'Ã','à');
+UPDATE `game_event` SET description = replace(description,'àŠ','ê');
+UPDATE `game_event` SET description = replace(description,'à§','ç');
+UPDATE `game_event` SET description = replace(description,'â€œ','"');
+UPDATE `game_event` SET description = replace(description,'â€?','"');
+UPDATE `game_event` SET description = replace(description,'â€˜',"'");
+UPDATE `game_event` SET description = replace(description,'cÅ“','œ');
+UPDATE `game_event` SET description = replace(description,'à¹','ù');
+UPDATE `game_event` SET description = replace(description,'Â',',');
+UPDATE `game_event` SET description = replace(description,'Å“','œ');
+
+
+
+
+
---
+++ trunk/Updates/434/zzz_Fix accents_points_of_interest.sql
@@ -0,0 +1,32 @@
+-- For those who have a problems with the accents.
+-- @ made by sulbutx + Firelook
+
+
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã¢','â');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã©','é');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã»','û');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã¯','ï');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ãª','ê');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã¨','è');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã®','î');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã´','ô');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'ÃƒÂ©','é');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã€','à');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'â€™',"'");
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'â€“','à');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'â€¦','!');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Ã','à');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'àŠ','ê');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'à§','ç');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'â€œ','"');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'â€?','"');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'â€˜',"'");
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'cÅ“','œ');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'à¹','ù');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Â',',');
+UPDATE `locales_points_of_interest` SET icon_name_loc2 = replace(icon_name_loc2,'Å“','œ');
+
+
+
+
+
